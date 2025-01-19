d1ff7d00cc4348b079f437bcc4a48f4946c65847
prajwal2403 <prajwal.p.khobragade@gmail.com>
2025-01-19 18:50:17 +0530

    front2

--- main.py
+++ main.py
@@ -102,13 +102,19 @@ port = int(os.environ.get("PORT", 8000))   # Default to 8000 if PORT is not set
 
 
 # Allow CORS for frontend
+origins = [
+    "https://ml-web-zeta.vercel.app"  # Add your frontend URL here
+]
+
+# Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:4173","https://ml-n4uxhum84-prajwal2403s-projects.vercel.app/"],  # React frontend URL
+    allow_origins=origins,  # Allow specific origins
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_methods=["*"],  # Allow all HTTP methods (e.g., GET, POST, PUT, DELETE)
     allow_headers=["*"],  # Allow all headers
 )
+class F
 
 class FeatureEngineeringRequest(BaseModel):
     method: str
